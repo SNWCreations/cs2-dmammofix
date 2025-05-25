25b955fd034b4cb8bc37d9ec70a7bf4235be735f
ZX夏夜之风 <snwcreations@qq.com>
2025-05-25 18:20:08 +0800

    init: initial commit

---
+++ DMAmmoFix.cs
@@ -0,0 +1,40 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Cvars;
+using Microsoft.Extensions.Logging;
+
+namespace cs2_dmammofix;
+
+public class DMAmmoFix : BasePlugin
+{
+    public override string ModuleName => "DM Ammo Fix";
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "SNWCreations";
+    public override string ModuleDescription => "Fix `sv_infinite_ammo 2` does not work in kus' modded server";
+
+    private readonly ConVar _gameType = ConVar.Find("game_type")!;
+    private readonly ConVar _gameMode = ConVar.Find("game_mode")!;
+
+    [GameEventHandler]
+    public HookResult OnWeaponReload(EventWeaponFire @event, GameEventInfo info)
+    {
+        var gameType = _gameType.GetPrimitiveValue<int>();
+        var gameMode = _gameMode.GetPrimitiveValue<int>();
+
+        if (gameType == 1 && gameMode == 2) // are we in Valve's DM mode?
+        {
+            var user = @event.Userid;
+            if (user != null)
+            {
+                var pawn = user.Pawn.Get()!.As<CCSPlayerPawn>();
+                var weapon = pawn.WeaponServices!.ActiveWeapon.Get()!;
+                var vdata = weapon.VData!.As<CCSWeaponBaseVData>();
+                weapon.ReserveAmmo[0] = vdata.PrimaryReserveAmmoMax;
+                Utilities.SetStateChanged(weapon, "CBasePlayerWeapon", "m_pReserveAmmo");
+            }
+        }
+
+        return HookResult.Continue;
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# cs2-dmammofix
+
+A plugin which refills the reserved ammo of the weapon players use.
+
+This plugin is designed for [kus' modded server pack](https://github.com/kus/cs2-modded-server)
+
+Due to technical limitation, currently we can't fully simulate `sv_infinite_ammo 2`,
+ so we refill the reserved ammo when player shoots.
+
+This issue will be solved if we figured out how to insert a callback to be called
+ after a weapon got reloaded.
+
+## License
+
+Copyright (C) 2025 SNWCreations, Licensed under MIT License.
